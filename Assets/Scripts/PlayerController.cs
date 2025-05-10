205eace7ab574665e63acc4f28dc7f1b70ebcd64
WoojinChun <woojinchun3@gmail.com>
2025-05-10 04:28:53 -0700

    Fixed sprint bug and glide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,19 +36,20 @@ public class PlayerController : MonoBehaviour
 	private int _numberOfJumps;
 	[SerializeField] private int maxNumberOfJumps = 2;
 
-	[SerializeField] private float glideFallSpeed = -2f;
-	private bool _wantsToGlide;
+	#endregion
+	#region Variables: Glide
+	[SerializeField] private float glideGravityMultiplier = 0.5f; // how much to scale gravity when gliding
+	[SerializeField] private float maxGlideFallSpeed = -2f;       // the fastest downward velocity allowed while gliding
+	private bool _isGliding;
 
 	#endregion
 	
-	// Gets references on script startup to both the main camera and the CharacterController component attached to the player gameobject.
 	private void Awake()
 	{
 		_characterController = GetComponent<CharacterController>();
 		_mainCamera = Camera.main;
 	}
 
-	// Updates different aspects for the player based on the inputs by calling each functions every frame.
 	private void Update()
 	{
 		ApplyRotation();
@@ -56,89 +57,92 @@ public class PlayerController : MonoBehaviour
 		ApplyMovement();
 	}
 
-	
 	private void ApplyGravity()
 	{
-		// Resets downward velocity if its grounded and the velocity is less than 0
-		if (IsGrounded() && _velocity < 0.0f)
-		{
-			_velocity = -1.0f;
-		}
-		// If gliding, then reduce fall speed slowly
-		else if(_wantsToGlide)
-		{
-			_velocity += glideFallSpeed * Time.deltaTime;
-		}
-		// Else, apply normal gravity * mult
-		else
-		{
-			_velocity += _gravity * gravityMultiplier * Time.deltaTime;
-		}
-		
-		_direction.y = _velocity;
+    	// only stomp velocity when grounded and already moving down
+    	if (IsGrounded() && _velocity < 0f)
+    	{
+        	_velocity = -1f;
+        	_isGliding = false;
+    	}
+    	else
+    	{
+        	if (_isGliding && _velocity < 0f)
+        	{
+            	_velocity += _gravity * gravityMultiplier
+                         	* glideGravityMultiplier
+                         	* Time.deltaTime;
+            	_velocity = Mathf.Max(_velocity, maxGlideFallSpeed);
+        	}
+        	else
+        	{
+            	_velocity += _gravity * gravityMultiplier * Time.deltaTime;
+        	}
+    	}
+
+    	_direction.y = _velocity;
 	}
+
 	
 	private void ApplyRotation()
 	{
-		// If there's no input then ignore rest of code
 		if (_input.sqrMagnitude == 0) return;
 
-		// Converts the input direction in relation to the camera angle
 		_direction = Quaternion.Euler(0.0f, _mainCamera.transform.eulerAngles.y, 0.0f) * new Vector3(_input.x, 0.0f, _input.y);
-		// Finds desired rotation given direction
 		var targetRotation = Quaternion.LookRotation(_direction, Vector3.up);
-		// Rotate the player towards the target rotation
+
 		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 	}
 
 	private void ApplyMovement()
 	{
-		// Determines the targets speed
-		var targetSpeed = movement.isSprinting ? movement.speed * movement.multiplier : movement.speed;
-		// Using MoveTowards, allows a smoother transition to the target speed.
-		movement.currentSpeed = Mathf.MoveTowards(movement.currentSpeed, targetSpeed, movement.acceleration * Time.deltaTime);
-		// Move the character using the controller
-		_characterController.Move(_direction * movement.currentSpeed * Time.deltaTime);
+    	// Determines the target horizontal speed
+    	var targetHorizontalSpeed = movement.isSprinting ? movement.speed * movement.multiplier : movement.speed;
+    	// Using MoveTowards, allows a smoother transition to the target horizontal speed.
+    	movement.currentSpeed = Mathf.MoveTowards(movement.currentSpeed, targetHorizontalSpeed, movement.acceleration * Time.deltaTime);
+
+    	// Calculate horizontal movement components, scaled by currentSpeed
+    	float moveX = _direction.x * movement.currentSpeed;
+    	float moveZ = _direction.z * movement.currentSpeed;
+
+    	// Vertical movement component is _direction.y (which is _velocity)
+    	// It is not scaled by horizontal movement.currentSpeed
+    	float moveY = _direction.y;
+
+    	// Construct the final movement vector
+    	Vector3 finalMoveVector = new Vector3(moveX, moveY, moveZ);
+
+    	// Apply the movement using the correctly constructed finalMoveVector, scaled by Time.deltaTime
+    	_characterController.Move(finalMoveVector * Time.deltaTime);
+	}
+
+	public void Glide(InputAction.CallbackContext ctx)
+	{
+    	if (ctx.started)      _isGliding = true;
+    	else if (ctx.canceled) _isGliding = false;
 	}
 
 	public void Move(InputAction.CallbackContext context)
 	{
-		// Called by the input system to update the movement input
 		_input = context.ReadValue<Vector2>();
-		// Updates horizontal direction
 		_direction = new Vector3(_input.x, 0.0f, _input.y);
 	}
-
+	
 	public void Jump(InputAction.CallbackContext context)
 	{
-		// Triggers the hump on input start
 		if (!context.started) return;
-		// If the character is in midair and is out of jumps upon input press, then glide.
-		if (!IsGrounded() && _numberOfJumps >= maxNumberOfJumps)
-		{
-			Debug.Log("Glide");
-			_wantsToGlide = true;
-			return;
-		} 
-		// Otherwise, it either starts the coroutine to reset the jump count whenever the player lands on the ground
-		// or it keeps track of the jumps, applies the jumpForce, and cancels the gliding.
-		else 
-		{ 
-			if (_numberOfJumps == 0) StartCoroutine(WaitForLanding());
+		if (!IsGrounded() && _numberOfJumps >= maxNumberOfJumps) return;
+		if (_numberOfJumps == 0) StartCoroutine(WaitForLanding());
 		
-			_numberOfJumps++;
-			_velocity = jumpPower;
-			_wantsToGlide = false;
-		}
+		_numberOfJumps++;
+		_velocity = jumpPower;
 	}
 
-	// Sets the sprinting flag based off the input (holding down shift or not)
 	public void Sprint(InputAction.CallbackContext context)
 	{
 		movement.isSprinting = context.started || context.performed;
 	}
 
-	// Waits for the player to leave the ground, hit the ground, and resets the jump counter after both events.
 	private IEnumerator WaitForLanding()
 	{
 		yield return new WaitUntil(() => !IsGrounded());
