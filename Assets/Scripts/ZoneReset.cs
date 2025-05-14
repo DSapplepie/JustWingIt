386051e4bb03430f48aea7d7ed473ed239c57dba
dsaito <122303121+DSapplepie@users.noreply.github.com>
2025-05-13 17:24:20 -0700

    Added Comments / Finished Alpha

--- Assets/Scripts/ZoneReset.cs
+++ Assets/Scripts/ZoneReset.cs
@@ -5,7 +5,7 @@ public class ZoneReset : MonoBehaviour
     // A tag to identify the player
     [SerializeField] string playerTag = "Player";
     [SerializeField] private Transform spawnPoint;
-    // If the collision is caused by a other.gameObject that has the player tag then set the player's parent to the platform so they move with it.
+    // If the collision is caused by a other.gameObject that has the player tag then reset (transform) the player's position to the spawnPoint.
     private void OnTriggerEnter(Collider other){
         if(other.gameObject.tag.Equals(playerTag)){
             other.transform.position = spawnPoint.position;
