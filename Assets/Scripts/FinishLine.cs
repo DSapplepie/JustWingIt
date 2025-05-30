583b3bd114906fa05dd58b5b4d479ba02ccecd0c
dsaito <122303121+DSapplepie@users.noreply.github.com>
2025-05-29 20:56:18 -0700

    Quick Modification to Finish Line + Script

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,10 +5,14 @@ public class FinishLine : MonoBehaviour
     // A tag to identify the player
     [SerializeField] string playerTag = "Player";
 
-    private void OnTriggerEnter(Collider other){
-        if(other.gameObject.tag.Equals(playerTag)){
-            CheckpointManager.Instance.SetCheckpoint(transform.position); // Set the checkpoint to the finish line position
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals(playerTag))
+        {
+            CheckpointManager.Instance.SetCheckpoint(Vector3.zero); // Set the checkpoint to the starting point rather than setting the checkpoint to the finish line.
             Debug.Log("Player has reached the finish line!");
+            other.transform.position = Vector3.zero; //Reset the player back to the very beginning
         }
     }
 }
